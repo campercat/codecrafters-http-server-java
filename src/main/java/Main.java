8aa39b769c495700aac762977ac39c937383768e
Suey Chen <xchen3@wayfair.com>
2024-06-02 22:39:46 -0700

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
@@ -16,8 +19,16 @@ public class Main {
        // ensures that we don't run into 'Address already in use' errors
        serverSocket.setReuseAddress(true);
        clientSocket = serverSocket.accept(); // Wait for connection from client.
-       clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-       System.out.println("accepted new connection");
+       BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+       String line;
+       while((line = reader.readLine()) != null) {
+         System.out.println(line);
+         if (line.contains(" / ")) {
+           clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+         } else {
+           clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+         }
+       }
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
